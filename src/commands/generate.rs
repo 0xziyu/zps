bdb498dec55d4b5c9459fafa44d3e6c9d2fdb0e6
紫雨 <0xziyu@tutanota.com>
2025-05-23 23:26:32 -0400

    generate command

---
+++ src/commands/generate.rs
@@ -0,0 +1,34 @@
+use eyre::{Result, eyre};
+use rand::{rng, seq::IndexedRandom};
+use tracing::info;
+
+pub const DEFAULT_PASSWORD_LENGTH: usize = 12;
+const SYMBOLS: &[u8] = b"!@#$%^&*()_+-=[]{}|;:',.<>/?";
+
+// This function will be called by `add` and `generate` commands
+pub fn generate_password_internal(length: usize, no_symbols: bool) -> String {
+    let mut rng = rng();
+    let mut char_pool: Vec<char> = ('a'..='z').chain('A'..='Z').chain('0'..='9').collect();
+
+    if !no_symbols {
+        char_pool.extend(SYMBOLS.iter().map(|&s| s as char));
+    }
+
+    if char_pool.is_empty() {
+        return String::new();
+    }
+
+    let password = (0..length)
+        .map(|_| *char_pool.choose(&mut rng).unwrap())
+        .collect();
+    info!("Generated password: {}", password);
+    password
+}
+
+pub fn handle_generate(length: usize, no_symbols: bool) -> Result<()> {
+    if length == 0 {
+        return Err(eyre!("Password length cannot be zero."));
+    }
+    generate_password_internal(length, no_symbols);
+    Ok(())
+}
